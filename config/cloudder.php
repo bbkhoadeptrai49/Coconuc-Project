573385d0a43beac186201dc4c93d3d8971b46039
Truong072348 <47754112+Truong072348@users.noreply.github.com>
2019-11-11 05:15:59 +0700

    11/11

--- config/cloudder.php
+++ config/cloudder.php
@@ -12,12 +12,12 @@ return [
     |
     */
 
-    'cloudName'  => env('CLOUDINARY_CLOUD_NAME'),
-    'baseUrl'    => env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/coconuc-images'.env('CLOUDINARY_CLOUD_NAME')),
-    'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/coconuc-images'.env('CLOUDINARY_CLOUD_NAME')),
-    'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/coconuc-images'.env('CLOUDINARY_CLOUD_NAME')),
-    'apiKey'     => env('CLOUDINARY_API_KEY'),
-    'apiSecret'  => env('CLOUDINARY_API_SECRET'),
+    'cloudName'  => env('CLOUDINARY_CLOUD_NAME', 'coconuc-images'),
+    'baseUrl'    => env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
+    'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
+    'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/'.env('CLOUDINARY_CLOUD_NAME')),
+    'apiKey'     => env('CLOUDINARY_API_KEY','688668697348737'),
+    'apiSecret'  => env('CLOUDINARY_API_SECRET','8szVk-s9J6lJUa8B7ZAMxkyrHdA'),
 
     'scaling'    => [
         'format' => 'png',
